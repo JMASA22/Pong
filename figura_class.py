95be1708893addb06c09c1b6bc42f3014314a3d5
JMASA22 <janamasayos@gmail.com>
2022-12-28 13:01:34 +0100

    pong3

--- figura_class.py
+++ figura_class.py
@@ -1,3 +1,5 @@
+#k8 2:27:45
+
 import pygame as pg
 
 class Pilota:
@@ -8,29 +10,67 @@ class Pilota:
         self.color = color
         self.vx = vx
         self.vy = vy
+        self.contadorDrt = 0
+        self.contadorEsq = 0
+        font = pg.font.Font(None,40) 
 
+        
     def dibuixar (self,pantalla):
         pg.draw.circle (pantalla, self.color, (self.pos_x, self.pos_y), self.radio)
 
     def moure (self, xmax=800, ymax=600):
         self.pos_x += self.vx
         self.pos_y += self.vy
+
+        print ("posició X:",self.pos_x+self.radio)
+        print ("posició Y:",self.pos_y+self.radio)
+
         #canviem pq si es maca gol a un costat reboti exactament pel sentit contari
+        if self.pos_y >= ymax-self.radio or self.pos_y < 0+self.radio:
+            self.vy *= -1
 
         #la pilota desapareix uns pocs segons quan fa gol (límit de y)
-        if self.pos_x >= xmax+self.radio*10 or self.pos_x < 0+self.radio*10:
-            #contar gol
-            
+        if self.pos_x >= xmax+self.radio*10: #límit dr
+            self.contadorEsq += 1
             self.vx *= -1 
             self.yx *= -1 
 
-"""
-        if self.pos_x >= xmax-self.radio or self.pos_x < 0+self.radio:
+        if self.pos_x < 0+self.radio*10: #límit esq
+            self.contadorDrt += 1
             self.vx *= -1 
+            self.yx *= -1 
 
-        if self.pos_y >= ymax-self.radio or self.pos_y < 0+self.radio:
-            self.vy *= -1 
-"""
+    def marcador (self,pantalla_principal):
+        marcadorEsq = self.font.render("jugador 1"+str(self.contadorDrt), 0, (255,255,0))
+        marcadorDrt = self.font.render("jugador 2"+str(self.contadorEsq), 0, (255,255,0))
+
+        pantalla_principal.blit(marcadorDrt,(200,50))
+        pantalla_principal.blit(marcadorEsq,(600,50))
+
+    def posX (self):
+        return self.pos_x+self.radio
+    def posY (self):
+        return self.pos_y+self.radio
+   
+    def Esq (self):
+        if self.pos_x < 400:
+            return True
+        return False
+   
+    def Drt (self):
+        if self.pos_x > 400:
+            return True
+        return False
+
+    def Up (self):
+        if self.pos_y < 300:
+            return True
+        return False  
+
+    def Down (self):
+        if self.pos_y > 300:
+            return True
+        return False  
 
 class Raqueta:
     def __init__(self, pos_x, pos_y, w=20, h=100, color=(255,255,255), vx=1, vy=1):
@@ -66,4 +106,3 @@ class Raqueta:
         if self.pos_y < self.h // 2:
             self.pos_y = self.h // 2
 """
-
--- main.py
+++ main.py
@@ -14,16 +14,15 @@ cronometre = pg.time.Clock()
 pilota = Pilota (400,300)
 raqueta1 = Raqueta (10,300)
 raqueta2 = Raqueta (790,300)
-contador_j1 =
-contador_j2 = 
 
 raqueta1.vy = 5
+pilota.vx = 4
 
 game_over = False
-    
+
 while not game_over:
     # imprimir els milisegosnq triguen els fotogrames actualment
-    vt = cronometre.tick(300) # variable per controlar velicitat entre fotogrames
+    vt = cronometre.tick(500) # variable per controlar velicitat entre fotogrames
     # print (vt)
 
     for eventos in pg.event.get():
@@ -31,15 +30,17 @@ while not game_over:
         if eventos.type == pg.QUIT:
             game_over = True
 
-    raqueta1.moure (pg.K_w,pg.K_s)
-    raqueta2.moure (pg.K_UP,pg.K_DOWN)
-
-    #pilota.moure ()
-
     pantalla_principal.fill((0,130,95)) #pintar pantalla
+    pilota.marcador(pantalla_principal)
+
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=2) #pintar línia mig
 
-    #pilota.dibuixar (pantalla_principal)
+    pilota.dibuixar (pantalla_principal)
     raqueta1.dibuixar (pantalla_principal)
     raqueta2.dibuixar (pantalla_principal)
+
+    raqueta1.moure (pg.K_w,pg.K_s)
+    raqueta2.moure (pg.K_UP,pg.K_DOWN)
+    pilota.moure ()
+
     pg.display.flip()
